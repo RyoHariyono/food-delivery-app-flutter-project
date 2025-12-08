ff9c3afab105321e0ce21e4ed3664357e2567f58
lordbruh0 <ryohariyono17@gmail.com>
2025-12-08 21:11:28 +0700

    feat(Auth system): :lipstick: membuat halaman login
    membuat ui dan controllers untuk halaman login aplikasi

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,10 +1,97 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/components/my_button.dart';
+import 'package:food_delivery_app/components/my_textfield.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+class LoginPage extends StatefulWidget {
+  final void Function()? onTap;
+
+  LoginPage({super.key, required this.onTap});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  // text editing controllers
+  final TextEditingController emailController = TextEditingController();
+
+  final TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Center(
+        child: Column(
+          children: [
+            // logo
+            Icon(
+              Icons.lock_open_rounded,
+              size: 72,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+
+            const SizedBox(height: 25),
+
+            // message, app slogan
+            Text(
+              "Food Delivery App",
+              style: TextStyle(
+                fontSize: 16,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+            ),
+
+            const SizedBox(height: 25),
+
+            // email textfield
+            MyTextField(
+              controller: emailController,
+              hintText: "Email",
+              obscureText: false,
+            ),
+
+            const SizedBox(height: 10),
+
+            //password textfield
+            MyTextField(
+              controller: passwordController,
+              hintText: "Password",
+              obscureText: true,
+            ),
+
+            const SizedBox(height: 10),
+
+            //sign in button
+            MyButton(text: "Sign In", onTap: () {}),
+
+            const SizedBox(height: 10),
+
+            Row(
+              children: [
+                Text(
+                  "Not a mamber?",
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.inversePrimary,
+                  ),
+                ),
+                const SizedBox(width: 4),
+                GestureDetector(
+                  onTap: widget.onTap,
+                  child: Text(
+                    "Register now",
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            // not a member? register now
+          ],
+        ),
+      ),
+    );
   }
 }
