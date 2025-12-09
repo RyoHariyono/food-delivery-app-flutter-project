50cf84141defb1dae48da1c8453aefc1e95dc4f0
lordbruh0 <ryohariyono17@gmail.com>
2025-12-09 07:19:55 +0700

    feat(Navigation pages): :rocket: Membuat drawer navigation
    Membuat sebuah drawer sebagai media navigation antara pages

---
+++ lib/components/my_drawer.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery_app/components/my_drawer_tile.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(25, 100.0, 25, 25),
+        child: Column(
+          children: [
+            // app logo
+            Icon(
+              Icons.lock_open_rounded,
+              size: 40,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+            SizedBox(height: 25),
+            Divider(color: Theme.of(context).colorScheme.secondary),
+
+            // home list tile
+            MyDrawerTile(
+              text: 'Home',
+              icon: Icons.home,
+              onTap: () => Navigator.pop(context),
+            ),
+
+            // settings list tile
+            MyDrawerTile(
+              text: 'Settings',
+              icon: Icons.settings,
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SettingsPage()),
+                );
+              },
+            ),
+
+            const Spacer(),
+            // logout list tile
+            MyDrawerTile(text: 'Logout', icon: Icons.logout, onTap: () {}),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/components/my_drawer.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,6 +13,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Home Page"), centerTitle: true),
+      drawer: MyDrawer(),
     );
   }
 }
