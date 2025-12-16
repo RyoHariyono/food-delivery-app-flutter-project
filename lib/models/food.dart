29d45368b5bea8e735efe98f61bff2a9b2c000a6
lordbruh0 <ryohariyono17@gmail.com>
2025-12-16 07:55:51 +0700

    feat(databases): :sparkles: membuat models untuk makanan
    membuat models untuk makanan yang berisi nama, deskripsi, gambar, harga, category, dan sides, sides. Penambahan foto category makanan.

---
+++ lib/models/food.dart
@@ -0,0 +1,29 @@
+// food item
+class Food {
+  final String name;
+  final String description;
+  final String imagePath;
+  final double price;
+  final FoodCategory category;
+  List<Addon> availabelAddons;
+
+  Food({
+    required this.name,
+    required this.description,
+    required this.imagePath,
+    required this.price,
+    required this.category,
+    required this.availabelAddons,
+  });
+}
+
+// food categories
+enum FoodCategory { burgers, salads, desserts, drinks }
+
+// food addons
+class Addon {
+  final String name;
+  final double price;
+
+  Addon({required this.name, required this.price});
+}
